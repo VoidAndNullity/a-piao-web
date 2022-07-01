4ba276a62d81a5d3d34a78459e35e84c9c900615
sgdygb <523219482@qq.com>
2022-07-01 18:22:36 +0800

    删除多余的

--- babel.config.js
+++ babel.config.js
@@ -9,11 +9,6 @@ module.exports = {
         "libraryName": "element-ui",
         "styleLibraryName": "theme-chalk"
       }
-    ],
-    ["import", {
-      "libraryName": "muse-ui",
-      "libraryDirectory": "lib",
-      "camel2DashComponentName": false
-    }]
+    ]
   ]
 }
